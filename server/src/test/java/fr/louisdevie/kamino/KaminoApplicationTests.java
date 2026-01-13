dc8c4c347d9a3f4f81adacbfda38c45f48ea82d4
Louis DEVIE <louisdevie.contact@gmail.com>
2026-01-13 22:18:08 +0100

    add demo project

---
+++ server/src/test/java/fr/louisdevie/kamino/KaminoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.louisdevie.kamino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
